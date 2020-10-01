f020bdd6d6448e470904ba0fd00501c4ec3577aa
Oleg-rb <oleg1107@rambler.ru>
2020-10-01 16:21:50 +0300

    Update ARGV #3

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 gemspec
 
+gem 'coveralls', require: false
 gem 'rubocop'
 gem 'travis'
 gem 'rspec'
--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,16 +1,16 @@
 require "nmax/version"
 
 module NmaxOleg
-  def self.nmax(file, n)
-    if ARGV[2]
+  def self.nmax(n = ARGV[0].to_i)
+    if ARGV[1]
       puts "Enter only one argument"
       exit
     end
 
     numbers = []
-    file.each_line do |line|
+    $stdin.each_line do |line|
       line.scan(/[0-9]{1,1000}/).each{|i| numbers << i.to_i}
     end
-    numbers.uniq.sort.reverse[0...n.to_i]
+    puts numbers.uniq.sort.reverse[0...n]
   end
 end
